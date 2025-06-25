33a5d64066912b9265ab286d72766e5884b7234c
josh <jaaurellano@gbox.ncf.edu.ph>
2025-06-25 13:58:03 +0800

    Display the fetched quote

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,61 @@
 import { Card, Text } from '@rneui/themed';
-import { View } from "react-native";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from "react-native";
+
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 export default function Index() {
+
+const headers = {
+  accept:'application/json'
+}
+
+type QuotesData = {
+  content: string,
+  author: string,
+  authorSlug: string,
+  length: number
+}
+
+const [quotesData, setQuotesData] = useState<QuotesData[]|null>(null)
+
+const fetchQuotes = async () => {
+  const response = await axios.get(`${apiUrl}/quote`,{headers:headers})
+  setQuotesData(response.data)
+} 
+
+useEffect (() =>{
+  fetchQuotes()
+},[])
+
   return (
-    <View>
-      <Card>
-        <Text h4> Your daily quote</Text>
+    <View style={styles.container}>
+      <Card containerStyle={styles.card}>
+        <View style={styles.quote}>
+        <Text>{quotesData?.[0]?.content}</Text>
+        <Text>- {quotesData?.[0]?.author}</Text>
+        </View>
       </Card>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  card:{
+    height: 250,
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  quote: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+
+})
